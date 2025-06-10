e124b5b9862d6c40d965b79e69678e454ced1e46
atharvpande77 <atharvpande@social-impact.in>
2025-06-11 02:52:46 +0530

    vercel issues 3

--- src/app.js
+++ src/app.js
@@ -15,6 +15,10 @@ app.use(express.urlencoded({ extended: false }))
 app.use(logger)
 app.use(cors)
 
+app.get('/', (req, res) => {
+    res.json({"status": "healthy"})
+})
+
 app.use("/api/v1/users", usersRouter)
 app.use("/api/v1/auth", authRouter)
 app.use("/api/v1/posts", postsRouter)
@@ -27,17 +31,12 @@ app.use( (req, res, next) => {
 })
 app.use(errorHandler)
 
-app.get('/', (req, res) => {
-    res.json({"status": "healthy"})
-})
+
 
 export default app
 
-// app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`);
-// })
 if (process.env.NODE_ENV !== 'production') {
-  const PORT = process.env.PORT || 3000;
+  const PORT = process.env.PORT || 5000;
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
