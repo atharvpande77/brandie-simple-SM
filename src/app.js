db138af725fc5877e11b9f9dda1c417f910fe8ed
atharvpande77 <atharvpande@social-impact.in>
2025-06-11 01:50:59 +0530

    Added cors headers

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import postsRouter from './routes/posts.js'
 import followRouter from './routes/follow.js'
 import logger from './middleware/logger.js';
 import errorHandler from './middleware/error.js';
+import cors from './middleware/cors.js'
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -12,6 +13,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(logger)
+app.use(cors)
 
 app.use("/api/v1/users", usersRouter)
 app.use("/api/v1/auth", authRouter)
